3dfd7c9e0b7fc9981422c871d9d3d48c1c62e2a5
cheriahkay <cheriahgat@gmail.com>
2024-06-21 10:26:48 +0300

    initial commit

---
+++ michellefavour/src/App.js
@@ -0,0 +1,18 @@
+
+// src/App.js
+import React from 'react';
+import Gallery from './Components/Gallery';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Flower Picture Gallery ? </h1>
+      </header>
+      <Gallery />
+    </div>
+  );
+}
+
+export default App;
